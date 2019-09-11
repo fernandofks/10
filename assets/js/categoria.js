421226dfbbe5fdeeab027f2783e33eef446c8e98
Sophia <sophia.shigueoka@gmail.com>
2019-09-11 09:06:11 -0300

    adicionamos categoria.js

---
+++ assets/js/categoria.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let db = coDesConnect("https://projeto-afs.firebaseio.com/");
+  db.download("/portfolio/", function(data) {
+    for (let key in data) {
+      console.log(data[key]);
+    }
+  });
+});
